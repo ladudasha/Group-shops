05cc4cf272607162c1aa0bcb08b4b4046ca86548
ladudasha <darya.gameza@gmail.com>
2021-10-14 23:03:55 +0300

    Sructure Main

--- src/App.js
+++ src/App.js
@@ -1,44 +1,21 @@
 import './App.css';
-
 import { BrowserRouter } from 'react-router-dom';
-import PopularProducts from './components/popular-products/PopularProducts';
 import Header from './components/common/header/Header.jsx';
 import Content from './components/common/content/Content.jsx';
 import Footer from './components/common/footer/Footer.jsx';
 
 
 
-
-
-
-
 function App() {
-
-   
 
     return ( 
     <BrowserRouter >
-
-        <div >
-
+        <div>
             <Header/>
             <Content/>
             <Footer/>
-
-        
-        
-
         </div>
-
-        <div >
-        <PopularProducts/>
-        </div>
-
-
-
-
-
-        </BrowserRouter>
+    </BrowserRouter>
     );
 }
 
---
+++ src/components/product-card/ProductCard.jsx
@@ -0,0 +1,11 @@
+import s from "./ProductCard.module.scss";
+
+
+
+function ProductCard(props) {
+    return ( 
+        <div className={s.productCard}></div>
+    );
+}
+
+export default ProductCard;
