3e9050c9e97871645fcc43141d502d4f07a35094
Klimovich Yauhenia <zh.shuravina@gmail.com>
2021-10-25 13:51:01 +0300

    catalog structure commit

--- src/components/catalog/Catalog.jsx
+++ src/components/catalog/Catalog.jsx
@@ -4,7 +4,17 @@ import s from "./Catalog.module.scss";
 
 function Catalog(props) {
     return ( 
-        <div className={s.catalog}></div>
+        <div className={s.catalog}>
+            <div className={s.container}>
+                <div className={s.catalogContent}>
+                    <div className={s.catalogTitleBlock}>catalogTitle+button</div>
+                    <sidebar className={s.filterProductsBlock}>filterBlock</sidebar>
+                    <div className={s.catalogCards}>Catalog: ProductsCard*12</div>
+                    <div className={s.paginationPages}>pagination pages</div>
+                </div>
+                
+            </div>
+        </div>
     );
 }
 
