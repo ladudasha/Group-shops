91daed2e407449e29d5890e90f2d51bfcd6d815f
Klimovich Yauhenia <zh.shuravina@gmail.com>
2021-10-26 12:36:59 +0300

    breadcrumbs include in catalogpages commit

--- src/components/catalog/Catalog.jsx
+++ src/components/catalog/Catalog.jsx
@@ -1,5 +1,6 @@
-import s from "./Catalog.module.scss";
 
+import BasicBreadcrumbs from "../basic breadcrumbs/BasicBreadcrumbs";
+import s from "./Catalog.module.scss";
 
 
 function Catalog(props) {
@@ -7,10 +8,16 @@ function Catalog(props) {
         <div className={s.catalog}>
             <div className={s.container}>
                 <div className={s.catalogContent}>
-                    <div className={s.catalogTitleBlock}>catalogTitle+button</div>
+                    <div className={s.catalogTop}>
+                        <BasicBreadcrumbs/>
+                        catalogTitle+button
+                        <h1 id="jetSkis">Гидроциклы</h1>
+                    </div>
+                        
+
                     <sidebar className={s.filterProductsBlock}>filterBlock</sidebar>
                     <div className={s.catalogCards}>Catalog: ProductsCard*12</div>
-                    <div className={s.paginationPages}>pagination pages</div>
+                    <div className={s.paginationPages}>pagination pages???</div>
                 </div>
                 
             </div>
