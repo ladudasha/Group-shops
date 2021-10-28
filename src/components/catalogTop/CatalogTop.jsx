cbca38669cf50674c77db8316188905a17bbe34e
ladudasha <darya.gameza@gmail.com>
2021-10-28 23:31:36 +0300

    filterBlock top

--- src/components/catalogTop/CatalogTop.jsx
+++ src/components/catalogTop/CatalogTop.jsx
@@ -1,4 +1,7 @@
+import FilterTopSelect from "../filter-top-select/FilterTopSelect";
 import s from "./CatalogTop.module.scss";
+import icon1 from "./../../assets/images/CatalogTop/icon1.svg";
+import icon2 from "./../../assets/images/CatalogTop/icon2.svg"
 
 
 function CatalogTop() {
@@ -6,13 +9,30 @@ function CatalogTop() {
     return ( 
         <div className={s.catalogTop}>
             <div className={s.container}>
-                <div className={s.catalogTop}>
+                
                     
                     <h1>Гидроциклы</h1>
+                    <div className={s.wrapper}>
+                        <div className={s.groupLeft}>
+                            <button>Полноприводные</button>
+                            <button>от 5000</button>
+                            <button>BRP</button>
+                            <button>еще</button>
+                        </div>
+
+                        <div className={s.groupRight}>
+                            <FilterTopSelect/>
+                            <button><img src={icon1} alt="" /></button>
+                            <button><img src={icon2} alt="" /></button>
+                        </div>
+
+                    </div>
+                   
+                
                 </div>
                     
             </div>
-        </div>
+       
     );
 }
 
