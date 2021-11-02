c8d3682925f3ae0d865272bd38074e4ffb67fdfb
Klimovich Yauhenia <zh.shuravina@gmail.com>
2021-11-02 14:34:48 +0300

    select top new structure and style  commit

--- src/components/filter-top-select/FilterTopSelect.jsx
+++ src/components/filter-top-select/FilterTopSelect.jsx
@@ -1,88 +1,145 @@
 import * as React from 'react';
-import { useTheme } from '@mui/material/styles';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
+import { makeStyles } from '@material-ui/core/styles';
 
-
-
-const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 8;
-const MenuProps = {
-  PaperProps: {
-    style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250,
-      
-    },
-  },
-};
-
-const names = [
-  'Oliver Hansen',
-  'Van Henry',
-  'April Tucker',
-  'Ralph Hubbard',
-  'Omar Alexander',
-  'Carlos Abbott',
-  'Miriam Wagner',
-  'Bradley Wilkerson',
-  'Virginia Andrews',
-  'Kelly Snyder',
-];
-
-function getStyles(name, personName, theme) {
-  return {
-    fontWeight:
-      personName.indexOf(name) === -1
-        ? theme.typography.fontWeightRegular
-        : theme.typography.fontWeightMedium,
-  };
-}
-
-export default function FilterTopSelect() {
-  const theme = useTheme();
-  const [personName, setPersonName] = React.useState([]);
+export default function SelectLabels(props) {
+  const [age, setAge] = React.useState('');
 
   const handleChange = (event) => {
-    const {
-      target: { value },
-    } = event;
-    setPersonName(
-      // On autofill we get a the stringified value.
-      typeof value === 'string' ? value.split(',') : value,
-    );
+    setAge(event.target.value);
   };
+
+  const myTopSelect = makeStyles((theme) =>({
+    root: {
+        height:33,
+        fontFamily:'SFProDisplayRegular',
+        fontWeight: 400,
+        fontSize: 14,
+        color:'#2F3035',
+        borderRadius:0,
+      //   '& .MuiSelect-icon': {
+          
+      // },
+    },
+}),
+);
+
+  const classes = myTopSelect();
+
 
   return (
     <div>
-      <FormControl sx={{ m: 1, width: 178, hight: 33,}}>
-        <InputLabel id="demo-multiple-name-label">По полулярности</InputLabel>
+      <FormControl sx={{ m: 1, minWidth: 178 }}>
         <Select
-          labelId="demo-multiple-name-label"
-          id="demo-multiple-name"
-          multiple
-          value={personName}
+          value={age}
           onChange={handleChange}
-          input={<OutlinedInput label="Name" />}
-          MenuProps={MenuProps}
+          displayEmpty
+          className={classes.root}
         >
-          {names.map((name) => (
-            <MenuItem
-              key={name}
-              value={name}
-              style={getStyles(name, personName, theme)}
-            >
-              {name}
-            </MenuItem>
-          ))}
+          <MenuItem value={10}>По популярности</MenuItem>
+          <MenuItem value={20}>По цене с min</MenuItem>
+          <MenuItem value={30}>По цене с max</MenuItem>
+          <MenuItem value={40}>По рейтингу</MenuItem>
         </Select>
       </FormControl>
     </div>
   );
 }
+
+
+
+
+
+
+// 
+{/* 
+          import * as React from 'react'; */}
+// import { useTheme } from '@mui/material/styles';
+// import OutlinedInput from '@mui/material/OutlinedInput';
+// import InputLabel from '@mui/material/InputLabel';
+// import MenuItem from '@mui/material/MenuItem';
+// import FormControl from '@mui/material/FormControl';
+// import Select from '@mui/material/Select';
+
+
+
+// const ITEM_HEIGHT = 48;
+// const ITEM_PADDING_TOP = 8;
+// const MenuProps = {
+//   PaperProps: {
+//     style: {
+//       maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+//       width: 250,
+      
+//     },
+//   },
+// };
+
+// const names = [
+//   'Oliver Hansen',
+//   'Van Henry',
+//   'April Tucker',
+//   'Ralph Hubbard',
+//   'Omar Alexander',
+//   'Carlos Abbott',
+//   'Miriam Wagner',
+//   'Bradley Wilkerson',
+//   'Virginia Andrews',
+//   'Kelly Snyder',
+// ];
+
+// function getStyles(name, personName, theme) {
+//   return {
+//     fontWeight:
+//       personName.indexOf(name) === -1
+//         ? theme.typography.fontWeightRegular
+//         : theme.typography.fontWeightMedium,
+//   };
+// }
+
+// export default function FilterTopSelect() {
+//   const theme = useTheme();
+//   const [personName, setPersonName] = React.useState([]);
+
+//   const handleChange = (event) => {
+//     const {
+//       target: { value },
+//     } = event;
+//     setPersonName(
+//       // On autofill we get a the stringified value.
+//       typeof value === 'string' ? value.split(',') : value,
+//     );
+//   };
+
+//   return (
+//     <div>
+//       <FormControl sx={{ m: 1, width: 178, hight: 33,}}>
+//         <InputLabel id="demo-multiple-name-label">По полулярности</InputLabel>
+//         <Select
+//           labelId="demo-multiple-name-label"
+//           id="demo-multiple-name"
+//           multiple
+//           value={personName}
+//           onChange={handleChange}
+//           input={<OutlinedInput label="Name" />}
+//           MenuProps={MenuProps}
+//         >
+//           {names.map((name) => (
+//             <MenuItem
+//               key={name}
+//               value={name}
+//               style={getStyles(name, personName, theme)}
+//             >
+//               {name}
+//             </MenuItem>
+//           ))}
+//         </Select>
+//       </FormControl>
+//     </div>
+//   );
+// }
 
 
 // import * as React from 'react';
