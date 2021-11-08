18e4ee2f7a4e483988c3ac2f9c0438fda269db9c
Klimovich Yauhenia <zh.shuravina@gmail.com>
2021-11-08 22:07:27 +0300

    new style select top and include new svg arrow  commit

--- src/components/filter-top-select/FilterTopSelect.jsx
+++ src/components/filter-top-select/FilterTopSelect.jsx
@@ -15,19 +15,22 @@ export default function SelectLabels(props) {
 
   const myTopSelect = makeStyles((theme) =>({
     root: {
-        height:33,
-        fontFamily:'SFProDisplayRegular',
-        fontWeight: 400,
-        fontSize: 14,
-        color:'#2F3035',
-        borderRadius:0,
-        paddingLeft: 15,
-      
-       
-      //   '& .MuiSelect-icon': {
-          
-      // },
-    },
+          height:28,
+          fontFamily:'SFProDisplayRegular',
+          fontWeight: 400,
+          fontSize: 14,
+          color:'#2F3035',
+          borderRadius:0,
+          paddingLeft: 15,
+          border: '1px solid #D7D9DF',
+          '& .MuiSelect-icon': {
+            right: 15,
+            top: 11,
+          },
+          '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+            border: '1px solid #D7D9DF',
+          },
+    }
 }),
 );
 
@@ -45,8 +48,15 @@ export default function SelectLabels(props) {
           displayEmpty
           autoWidth
           className={classes.root}
+          // inputProps={{ 'aria-label': 'Without label' }}
+          IconComponent={props => (<svg {...props} width="12" height="8" viewBox="0 0 12 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M10.6 0L6 4.6L1.4 0L0 1.4L6 7.4L12 1.4L10.6 0Z" fill="#D7D9DF"/>
+</svg>)}
         >
-          <MenuItem value={10}>По популярности</MenuItem>
+          <MenuItem value="">
+            По популярности
+          </MenuItem>
+          {/* <MenuItem value={10}>По популярности</MenuItem> */}
           <MenuItem value={20}>По цене с min</MenuItem>
           <MenuItem value={30}>По цене с max</MenuItem>
           <MenuItem value={40}>По рейтингу</MenuItem>
